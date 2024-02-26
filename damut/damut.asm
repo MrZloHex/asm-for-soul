a4997eb267ae43b6316f73b7de5c2aa26ee310c1
MrZloHex <zlo.alex.it@gmail.com>
2024-02-27 00:41:22 +0300

    fpu and avx basic stuff

--- damut/damut.asm
+++ damut/damut.asm
@@ -8,11 +8,61 @@
 
 		section   .text
 _start:		
-		call		clrscr
-		call		home
-		mov		rax, damut_msg
-		call		print
+		; call		clrscr
+		; call		home
+		; mov		rax, damut_msg
+		; call		print
+		; mov		rax, illumination
+		; call		print
+		; mov		rax, new_line
+		; call		print
+render_loop:	call		render
+		movaps		xmm0, [sine]
+		movaps		xmm1, [cosine]
+		; movaps		xmm0, [rotation_offset]
+		; movaps		xmm1, [rotation_delta]
+		; addps		xmm0, xmm1
+		; movaps		[rotation_offset], xmm0
+		; jmp		render_loop
 		jmp		exit_ok
+
+render:		call		calc_sc_ab
+		; THETA LOOP
+		mov		rax, theta
+		mov		rbx, 2
+		call		sin_cos
+		; PHI LOOP
+		mov		rax, phi
+		mov		rbx, 3
+		call		sin_cos
+		
+		ret
+
+calc_sc_ab:	; Calculate sina sinb cosa cosb
+		mov		rax, A
+		mov		rbx, 0
+		call		sin_cos
+		mov		rax, B
+		mov		rbx, 1
+		call		sin_cos
+		ret
+
+
+sin_cos:	; RAX - address of var ; RBX - offset of sine and cosine var
+		; push		rcx
+		mov		rcx, rbx
+		add		rbx, sine
+		add 		rcx, cosine
+		fld		dword [rax]
+		fsin
+		fst		dword [rbx]
+		fld		dword [rax]
+		fcos
+		fst		dword [rcx]
+		; pop		rcx
+		ret
+
+
 
 
 
@@ -25,10 +75,64 @@ exit_err:	mov		rax, 60
 		syscall
 
 
+render_frame:	
+		ret
+
 
 		section   .bss
-buf:		resb 		32
+; sine:
+; 	sinA:	resd		1
+; 	sinB:	resd		1
+; 	sinThe:	resd		1
+; 	sinPhi:	resd		1
+; cosine:
+; 	cosA:	resd		1
+; 	cosB:	resd		1
+; 	cosThe:	resd		1
+; 	cosPhi:	resd		1
+
 
 		section   .data
+sine:
+	sinA:	dd		0
+	sinB:	dd		0
+	sinThe:	dd		0
+	sinPhi:	dd		0
+cosine:
+	cosA:	dd		0
+	cosB:	dd		0
+	cosThe:	dd		0
+	cosPhi:	dd		0
+
+
+
+angles:
+	theta:	dd		0.00
+	phi:	dd		0.00
+		dq		0.00
+angles_delta:
+	deltaT:	dd		0.01
+	deltaP:	dd		0.01
+		dq		0.00
+
+rotation_offset:
+	A:	dd		1.00
+	B:	dd		1.00
+		dq		0.00
+rotation_delta:
+	deltaA:	dd		1.006
+	deltaB:	dd		2.004
+		dq		0.00
+	
+
 damut_msg:  	db		"DAMUT - DONUT in ASM", 10, 0
+illumination:	db		".,-~:;=!*#$@", 0
 new_line:	db		10,0
+screen:
+	width:	db		80
+	height: db		40
+
+pi:		dq		3.141592653589793238462
+		dq		0.00
+
+
